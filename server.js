5d794c38e8bdbfb311866a00406dfe91b197fd3f
martinsph <ph.mmartins@gmail.com>
2022-02-01 15:56:41 -0300

    add req 4 & fix eslint

--- server.js
+++ server.js
@@ -19,14 +19,16 @@ app.set('views', './views');
 
 const onlineUserList = {};
 
-/* eslint-disable */
-io.on('connection', async (socket) => {
-  console.log(`Usuário ${socket.id} conectou.`);
-  const defaultNick = socket.id.slice(0, 16);
-  onlineUserList[socket.id] = defaultNick;
+const sendHistory = async () => { 
+  const result = await listMessage(); return result; 
+};
 
-  // envia nickname default para client
-  socket.emit('defaultNickname', defaultNick);
+io.on('connection', async (socket) => {
+  // console.log(`Usuário ${socket.id} conectou.`);
+  // const defaultNick = socket.id.slice(0, 16);
+  onlineUserList[socket.id] = socket.id.slice(0, 16);
+
+  socket.emit('defaultNickname', socket.id.slice(0, 16));
   io.emit('userConnect', onlineUserList);
 
   socket.on('setNewNickname', ({ currentUser, newNickname }) => {
@@ -34,33 +36,27 @@ io.on('connection', async (socket) => {
     io.emit('setNickname', onlineUserList);
   });
 
-  // envia chatHistory para client
-  const sendHistory = async () => { 
-    const result = await listMessage(); return result; 
-  };
   socket.emit('chatHistory', await sendHistory());
-  // função para escutar as mensagens
+
   socket.on('message', async ({ nickname, chatMessage }) => {
     // date format function consultada em https://dockyard.com/blog/2020/02/14/you-probably-don-t-need-moment-js-anymore.
-    // cria timestamp e objeto menssagem
     const timestamp = (new Date()).toLocaleString('pt-BR').replace(/\//g, '-');
-    const message = { message: chatMessage, nickname, timestamp };
-    // salva no banco de dados a menssagem
-    await createMessage(message);
+    await createMessage({ message: chatMessage, nickname, timestamp });
     const newMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
-    // emite nova menssagem para tela
     io.emit('message', newMessage);
   });
-  // mensagem desconectar
+
   socket.on('disconnect', () => {
-    console.log(`Usuário ${socket.id} desconectou.`);
+    // console.log(`Usuário ${socket.id} desconectou.`);
     delete onlineUserList[socket.id];
     io.emit('userDisconnect', onlineUserList);
   });
 });
+
 app.get('/', (_req, res) => {
   res.render('webchat');
 });
+
 http.listen(EXPRESS_PORT, () => {
   console.log(`Servidor ouvindo na porta ${EXPRESS_PORT}`);
 });
